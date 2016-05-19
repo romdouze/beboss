c650443df5bf9aa80ecb2c2a4136716794df68bc
rom.douze@gmail.com <rom.douze@gmail.com>
2016-05-19 02:52:54 +0200

    .

--- src/ngr/KiKi/beboss_reloaded/data/XMLHandler.java
+++ src/ngr/KiKi/beboss_reloaded/data/XMLHandler.java
@@ -11,7 +11,25 @@ package ngr.KiKi.beboss_reloaded.data;
  */
 public class XMLHandler
 {
-    private static final String XML_CHIFFRE_AFFAIRE = "Chiffre-affaire";
-    private static final String XML_TRAVAUX_EN_COURS_INITIAUX = "Travaux-en-cours-initiaux";
-    private static final String XML_TRAVAUX_EN_COURS_FINAUX = "Travaux-en-cours-finaux";
+
+    private static final String XML_CHIFFRE_AFFAIRE = "chiffre-affaire";
+    private static final String XML_TRAVAUX_EN_COURS_INITIAUX = "travaux-en-cours-initiaux";
+    private static final String XML_TRAVAUX_EN_COURS_FINAUX = "travaux-en-cours-finaux";
+    private static final String XML_CHARGES_FIXES = "charges-fixes";
+    private static final String XML_CHARGES_VARIABLES = "charges-variables";
+    private static final String XML_STOCK_FINAL = "stock-final";
+    private static final String XML_ACHATS_CONSOMMES = "achats-consommes";
+    private static final String XML_ACHATS_NON_STOCKABLES = "achats-non-stockables";
+    private static final String XML_SERVICES_EXTERIEURS = "services-exterieurs";
+    private static final String XML_IMPOTS_ET_TAXES = "impots-et-taxes";
+    private static final String XML_CHARGES_DE_PERSONNEL = "charges-de-personnels";
+    private static final String XML_COTISATIONS_PERSONNELLES = "cotisations-personnelles";
+    private static final String XML_DOTATION_AMORTISSEMENTS = "dotation-amortissements";
+    private static final String XML_AUTRES_CHARGES_GESTION = "autres-charges-gestion";
+    private static final String XML_AUTRES_PRODUITS_GESTION = "autres-produits-gestion";
+    private static final String XML_PRODUITS_FINANCIERS = "produits-financiers";
+    private static final String XML_CHARGES_FINANCIERES = "charges-financieres";
+    private static final String XML_PRODUITS_EXCEPTIONNELS = "produits-exceptionnels";
+    private static final String XML_CHARGES_EXCEPTIONNELLES = "charges-exceptionnelles";
+    private static final String XML_IMPOT_BENEFICES = "impots-benefices";
 }
