b09841fa8e58def4aa16177aeaa0e8ad4d7e907a
rom.douze@gmail.com <rom.douze@gmail.com>
2016-05-19 02:23:03 +0200

    initial push

---
+++ src/ngr/KiKi/beboss_reloaded/data/XMLHandler.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ngr.KiKi.beboss_reloaded.data;
+
+/**
+ *
+ * @author KiKi
+ */
+public class XMLHandler
+{
+    private static final String XML_CHIFFRE_AFFAIRE = "Chiffre-affaire";
+    private static final String XML_TRAVAUX_EN_COURS_INITIAUX = "Travaux-en-cours-initiaux";
+    private static final String XML_TRAVAUX_EN_COURS_FINAUX = "Travaux-en-cours-finaux";
+}
